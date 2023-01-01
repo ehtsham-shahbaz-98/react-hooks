e034b39072196afbf9c4e7fee0829979b482493e
ehtsham-shahbaz-98 <ehtsham.shahbaz.1998@gmail.com>
2023-01-01 07:22:47 -0700

    update: 1st jan commit

---
+++ src/Practice/useRefPractice1.jsx
@@ -0,0 +1,27 @@
+import { useRef } from "react";
+
+function useRefPractice1() {
+  const inputRef = useRef();
+
+  const onSubmit = e => {
+    e.preventDefault();
+    console.log(123);
+  }
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          ref={inputRef}
+          className="form-control mb-2"
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default useRefPractice1;
