cde675f0c00add8c4fb08c18ee28070d9436d7ea
ehtsham-shahbaz-98 <ehtsham.shahbaz.1998@gmail.com>
2023-01-09 10:50:00 -0700

    update: 1st

---
+++ src/Practice/UseMemoExample.jsx
@@ -0,0 +1,9 @@
+import { useState, useEffect, useRef, useMemo } from "react";
+
+function UseMemoExample() {
+  const [number, setNumber] = useState(1);
+
+  return <div></div>;
+}
+
+export default UseMemoExample;
